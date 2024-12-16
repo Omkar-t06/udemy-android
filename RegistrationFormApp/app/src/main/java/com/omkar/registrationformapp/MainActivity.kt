aa81b9cd0bfe533c4d18d83a4a29bea3a0497a79
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-12-16 12:03:48 +0530

    setup registration screen

--- RegistrationFormApp/app/src/main/java/com/omkar/registrationformapp/MainActivity.kt
+++ RegistrationFormApp/app/src/main/java/com/omkar/registrationformapp/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.omkar.registrationformapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.omkar.registrationformapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -18,9 +25,30 @@ class MainActivity : AppCompatActivity() {
         }
 
         setupSpinner()
+        setupButton()
     }
 
     private fun setupSpinner() {
+        val titles = arrayOf("Miss", "Mrs", "Ms", "Mr", "Mx", "Dr", "Prof")
+        val titlesAdapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item, titles)
+        binding.spinnerTitle.adapter = titlesAdapter
+    }
 
+    private fun setupButton() {
+        binding.buttonCreateAccount.setOnClickListener {
+            createAccount()
+        }
+    }
+
+    private fun createAccount() {
+        val user = User(
+            binding.spinnerTitle.selectedItem as String,
+            binding.editTextFirstName.text.toString(),
+            binding.editTextLastName.text.toString(),
+            binding.editTextEmail.text.toString(),
+            binding.editTextPhoneNo.text.toString(),
+            binding.editTextPassword.text.toString()
+        )
+        val intent = Intent()
     }
 }
