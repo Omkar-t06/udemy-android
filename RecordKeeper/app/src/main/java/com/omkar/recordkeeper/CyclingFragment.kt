4787002100081032862c56fa69e47b862b30e2ab
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-12-18 18:51:25 +0530

    Build Cycling Edit intent

--- RecordKeeper/app/src/main/java/com/omkar/recordkeeper/CyclingFragment.kt
+++ RecordKeeper/app/src/main/java/com/omkar/recordkeeper/CyclingFragment.kt
@@ -1,17 +1,41 @@
 package com.omkar.recordkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.omkar.recordkeeper.databinding.FragmentCyclingBinding
 
 class CyclingFragment : Fragment() {
+
+    private lateinit var binding: FragmentCyclingBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_cycling, container, false)
+    ): View {
+        binding = FragmentCyclingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupClickListener()
+    }
+
+    private fun setupClickListener() {
+        binding.containerLongestRide.setOnClickListener { launchRunningRecordScreen("Longest Ride") }
+        binding.containerBiggestClimb.setOnClickListener { launchRunningRecordScreen("Biggest Climb") }
+        binding.containerBestAverageSpeed.setOnClickListener { launchRunningRecordScreen("Best Average Speed") }
+    }
+
+    private fun launchRunningRecordScreen(record: String) {
+        val intent = Intent(context, EditCyclingRecordActivity::class.java)
+        intent.putExtra("Record", record)
+        startActivity(intent)
     }
 }
